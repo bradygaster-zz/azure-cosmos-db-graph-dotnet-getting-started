ffe822a8c7171fe9046a619e69010ce96fa2ce4b
bradygaster <brady.gaster@gmail.com>
2017-08-22 00:34:37 -0700

    added comments

--- PeopleDataFunctions/Search.cs
+++ PeopleDataFunctions/Search.cs
@@ -27,8 +27,10 @@ namespace PeopleDataFunctions
         {
             log.Info("C# HTTP trigger function processed a request.");
 
+            // the person objects will be free-form in structure
             List<dynamic> results = new List<dynamic>();
 
+            // open the client's connection
             using (DocumentClient client = new DocumentClient(
                 new Uri(endpoint),
                 authKey,
@@ -38,17 +40,20 @@ namespace PeopleDataFunctions
                     ConnectionProtocol = Protocol.Tcp
                 }))
             {
+                // get a reference to the database the console app created
                 Database database = await client.CreateDatabaseIfNotExistsAsync(
                     new Database
                     {
                         Id = "graphdb"
                     });
 
+                // get an instance of the database's graph
                 DocumentCollection graph = await client.CreateDocumentCollectionIfNotExistsAsync(
                     UriFactory.CreateDatabaseUri("graphdb"),
                     new DocumentCollection { Id = "graphcollz" },
                     new RequestOptions { OfferThroughput = 1000 });
 
+                // build a gremlin query based on the existence of a name parameter
                 string name = req.GetQueryNameValuePairs()
                     .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
                     .Value;
@@ -57,11 +62,13 @@ namespace PeopleDataFunctions
                     ? client.CreateGremlinQuery<dynamic>(graph, string.Format("g.V('{0}')", name))
                     : client.CreateGremlinQuery<dynamic>(graph, "g.V()");
 
+                // iterate over all the results and add them to the list
                 while (query.HasMoreResults)
                     foreach (dynamic result in await query.ExecuteNextAsync())
                         results.Add(result);
             }
 
+            // return the list with an OK response
             return req.CreateResponse<List<dynamic>>(HttpStatusCode.OK, results);
         }
     }
